ff34d32e76d37376171631a8976b6f2cf09a821e
Simon Rosenberg <simon.rosenberg.sr1@roche.com>
2021-05-08 10:59:40 +0200

    add first test for type parsing

--- midifilter/musicxmlparser.py
+++ midifilter/musicxmlparser.py
@@ -1,2 +1,10 @@
+from lxml import etree
+
+
 def parse(input):
-    return ["quarter"]
+    et = etree.fromstring(input.encode("utf-8"))
+    type_elements = et.findall(".//type")
+    types = []
+    for type_element in type_elements:
+        types.append(type_element.text)
+    return types
--- tests/test.py
+++ tests/test.py
@@ -4,8 +4,7 @@ from midifilter.musicxmlparser import parse
 
 
 def test_return_type():
-    input = """
-<?xml version="1.0" encoding="UTF-8"?>
+    input = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
 <score-partwise version="3.1">
 <part id="P1">
@@ -15,12 +14,12 @@ def test_return_type():
         </note>
     </measure>
 </part>
+</score-partwise>
     """
     assert parse(input) == ["quarter"]
 
 def test_return_multiple_types():
-    input = """
-<?xml version="1.0" encoding="UTF-8"?>
+    input = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
 <score-partwise version="3.1">
 <part id="P1">
@@ -33,5 +32,6 @@ def test_return_multiple_types():
         </note>
     </measure>
 </part>
+</score-partwise>
     """
     assert parse(input) == ["quarter", "32nd"]
