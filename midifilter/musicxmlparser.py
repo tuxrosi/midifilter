8c60dca8fcbab1b6a745088f4eace30dbc5b444b
Simon Rosenberg <simon.rosenberg.sr1@roche.com>
2021-05-08 10:44:22 +0200

    add first files

---
+++ midifilter/musicxmlparser.py
@@ -0,0 +1,2 @@
+def parse(input):
+    return ["quarter"]
---
+++ tests/test.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append(".")
+from midifilter.musicxmlparser import parse
+
+
+def test_return_type():
+    input = """
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
+<score-partwise version="3.1">
+<part id="P1">
+    <measure>
+        <note>
+            <type>quarter</type>
+        </note>
+    </measure>
+</part>
+    """
+    assert parse(input) == ["quarter"]
+
+def test_return_multiple_types():
+    input = """
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
+<score-partwise version="3.1">
+<part id="P1">
+    <measure>
+        <note>
+            <type>quarter</type>
+        </note>
+        <note>
+            <type>32nd</type>
+        </note>
+    </measure>
+</part>
+    """
+    assert parse(input) == ["quarter", "32nd"]
